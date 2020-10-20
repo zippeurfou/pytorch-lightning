5942dea1c76f1f732189c4e6649b536aac7b88b8
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-10-20 20:27:20 +0200

    d

--- pytorch_lightning/accelerators/ddp_accelerator.py
+++ pytorch_lightning/accelerators/ddp_accelerator.py
@@ -182,7 +182,7 @@ class DDPAccelerator(Accelerator):
 
     def model_to_device(self, model, process_idx):
         self.trainer.root_gpu = process_idx
-        print(process_idx, self.trainer.root_gpu, torch.cuda.device_count())
+        print(process_idx, self.trainer.root_gpu, torch.cuda.device_count(), os.environ.get("CUDA_VISIBLE_DEVICES", "none"))
         torch.cuda.set_device(self.trainer.root_gpu)
         model.cuda(self.trainer.root_gpu)
 
