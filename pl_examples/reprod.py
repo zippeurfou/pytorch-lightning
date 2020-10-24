0647268fa4c444796a3327f90f74e21c996b2e9e
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-10-24 18:57:05 +0200

    update

--- pl_examples/reprod.py
+++ pl_examples/reprod.py
@@ -1,3 +1,24 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# --------------------------------------------
+# --------------------------------------------
+# --------------------------------------------
+# USE THIS MODEL TO REPRODUCE A BUG YOU REPORT
+# --------------------------------------------
+# --------------------------------------------
+# --------------------------------------------
 import glob
 import os
 from tempfile import TemporaryDirectory
@@ -94,24 +115,29 @@ def run_test():
 
     # model
     model = TestModel()
-    with TemporaryDirectory(dir=os.getcwd()) as tmp_dir:
-        trainer = Trainer(
-            default_root_dir=os.getcwd(),
-            max_epochs=2,
-            accelerator='ddp',
-            gpus=2,
-            checkpoint_callback=ModelCheckpoint(
-                dirpath=tmp_dir,
-                monitor='x',
-                mode='min',
-                save_top_k=1
-            )
+    tmp_dir = 'temp/'
+    if os.path.exists(tmp_dir):
+        os.rmdir(tmp_dir)
+
+    trainer = Trainer(
+        default_root_dir=os.getcwd(),
+        max_epochs=2,
+        accelerator='ddp',
+        gpus=2,
+        checkpoint_callback=ModelCheckpoint(
+            dirpath=tmp_dir,
+            monitor='x',
+            mode='min',
+            save_top_k=1
         )
-        trainer.fit(model, train_data, val_data)
-        checkpoints = list(sorted(glob.glob(os.path.join(tmp_dir, "*.ckpt"), recursive=True)))
-        print(checkpoints)
-        assert os.path.exists(
-            trainer.checkpoint_callback.best_model_path), f'Could not find checkpoint at rank {trainer.global_rank}'
+    )
+    trainer.fit(model, train_data, val_data)
+    checkpoints = list(sorted(glob.glob(os.path.join(tmp_dir, "*.ckpt"), recursive=True)))
+    print("checkpoints", checkpoints)
+    print(trainer.checkpoint_callback.best_model_path)
+    assert os.path.exists(
+        trainer.checkpoint_callback.best_model_path), f'Could not find checkpoint at rank {trainer.global_rank}'
+
 
 if __name__ == '__main__':
     run_test()
