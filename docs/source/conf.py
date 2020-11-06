126f7cc7fdf55e06ca65ad9ca9d33b28fbe6cfe7
Jirka Borovec <jirka@pytorchlightning.ai>
2020-11-06 16:07:02 +0100

    minor formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -308,8 +308,8 @@ MOCK_PACKAGES = []
 if SPHINX_MOCK_REQUIREMENTS:
     # mock also base packages when we are on RTD since we don't install them there
     MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, 'requirements.txt'))
-    MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, 'requirements/extra.txt'))
-    MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, 'requirements/loggers.txt'))
+    MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, 'requirements', 'extra.txt'))
+    MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, 'requirements', 'loggers.txt'))
 
 MOCK_MANUAL_PACKAGES = [
     'torchvision',
